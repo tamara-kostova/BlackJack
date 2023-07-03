bd4d8d157fc5d58c03de9a796172f07715896297
Tamara <90916955+tamara-kostova@users.noreply.github.com>
2023-07-03 12:10:54 +0200

    new version

--- BlackJack/Form1.Designer.cs
+++ BlackJack/Form1.Designer.cs
@@ -29,13 +29,33 @@
         private void InitializeComponent()
         {
             lblscorePlayer = new Label();
-            lblscoreComputer = new Label();
+            lblDealer = new Label();
             btnHit = new Button();
             btnStay = new Button();
             btnDeal = new Button();
-            splitContainer1 = new SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
-            splitContainer1.SuspendLayout();
+            dealerCard1 = new PictureBox();
+            dealerCard2 = new PictureBox();
+            playerCard2 = new PictureBox();
+            playerCard1 = new PictureBox();
+            playerCard4 = new PictureBox();
+            playerCard3 = new PictureBox();
+            playerCard6 = new PictureBox();
+            playerCard5 = new PictureBox();
+            playerCard8 = new PictureBox();
+            playerCard7 = new PictureBox();
+            playerCard9 = new PictureBox();
+            btnNewGame = new Button();
+            ((System.ComponentModel.ISupportInitialize)dealerCard1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dealerCard2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard9).BeginInit();
             SuspendLayout();
             // 
             // lblscorePlayer
@@ -47,18 +67,18 @@
             lblscorePlayer.TabIndex = 0;
             lblscorePlayer.Text = "label1";
             // 
-            // lblscoreComputer
+            // lblDealer
             // 
-            lblscoreComputer.AutoSize = true;
-            lblscoreComputer.Location = new Point(970, 9);
-            lblscoreComputer.Name = "lblscoreComputer";
-            lblscoreComputer.Size = new Size(59, 25);
-            lblscoreComputer.TabIndex = 1;
-            lblscoreComputer.Text = "label1";
+            lblDealer.AutoSize = true;
+            lblDealer.Location = new Point(784, 9);
+            lblDealer.Name = "lblDealer";
+            lblDealer.Size = new Size(59, 25);
+            lblDealer.TabIndex = 1;
+            lblDealer.Text = "label1";
             // 
             // btnHit
             // 
-            btnHit.Location = new Point(943, 241);
+            btnHit.Location = new Point(567, 374);
             btnHit.Name = "btnHit";
             btnHit.Size = new Size(112, 34);
             btnHit.TabIndex = 2;
@@ -68,7 +88,7 @@
             // 
             // btnStay
             // 
-            btnStay.Location = new Point(943, 293);
+            btnStay.Location = new Point(567, 435);
             btnStay.Name = "btnStay";
             btnStay.Size = new Size(112, 34);
             btnStay.TabIndex = 3;
@@ -78,48 +98,169 @@
             // 
             // btnDeal
             // 
-            btnDeal.Location = new Point(943, 135);
+            btnDeal.Location = new Point(703, 405);
             btnDeal.Name = "btnDeal";
             btnDeal.Size = new Size(112, 34);
             btnDeal.TabIndex = 5;
             btnDeal.Text = "Deal";
             btnDeal.UseVisualStyleBackColor = true;
             // 
-            // splitContainer1
+            // dealerCard1
             // 
-            splitContainer1.Location = new Point(0, 0);
-            splitContainer1.Name = "splitContainer1";
-            splitContainer1.Orientation = Orientation.Horizontal;
-            splitContainer1.Size = new Size(824, 449);
-            splitContainer1.SplitterDistance = 224;
-            splitContainer1.TabIndex = 6;
+            dealerCard1.Location = new Point(235, 12);
+            dealerCard1.Name = "dealerCard1";
+            dealerCard1.Size = new Size(88, 129);
+            dealerCard1.TabIndex = 6;
+            dealerCard1.TabStop = false;
+            // 
+            // dealerCard2
+            // 
+            dealerCard2.Location = new Point(371, 12);
+            dealerCard2.Name = "dealerCard2";
+            dealerCard2.Size = new Size(88, 129);
+            dealerCard2.TabIndex = 7;
+            dealerCard2.TabStop = false;
+            // 
+            // playerCard2
+            // 
+            playerCard2.Location = new Point(371, 374);
+            playerCard2.Name = "playerCard2";
+            playerCard2.Size = new Size(88, 129);
+            playerCard2.TabIndex = 9;
+            playerCard2.TabStop = false;
+            // 
+            // playerCard1
+            // 
+            playerCard1.Location = new Point(235, 374);
+            playerCard1.Name = "playerCard1";
+            playerCard1.Size = new Size(88, 129);
+            playerCard1.TabIndex = 8;
+            playerCard1.TabStop = false;
+            // 
+            // playerCard4
+            // 
+            playerCard4.Location = new Point(168, 184);
+            playerCard4.Name = "playerCard4";
+            playerCard4.Size = new Size(88, 129);
+            playerCard4.TabIndex = 11;
+            playerCard4.TabStop = false;
+            // 
+            // playerCard3
+            // 
+            playerCard3.Location = new Point(32, 184);
+            playerCard3.Name = "playerCard3";
+            playerCard3.Size = new Size(88, 129);
+            playerCard3.TabIndex = 10;
+            playerCard3.TabStop = false;
+            // 
+            // playerCard6
+            // 
+            playerCard6.Location = new Point(443, 184);
+            playerCard6.Name = "playerCard6";
+            playerCard6.Size = new Size(88, 129);
+            playerCard6.TabIndex = 13;
+            playerCard6.TabStop = false;
+            // 
+            // playerCard5
+            // 
+            playerCard5.Location = new Point(307, 184);
+            playerCard5.Name = "playerCard5";
+            playerCard5.Size = new Size(88, 129);
+            playerCard5.TabIndex = 12;
+            playerCard5.TabStop = false;
+            // 
+            // playerCard8
+            // 
+            playerCard8.Location = new Point(703, 184);
+            playerCard8.Name = "playerCard8";
+            playerCard8.Size = new Size(88, 129);
+            playerCard8.TabIndex = 15;
+            playerCard8.TabStop = false;
+            // 
+            // playerCard7
+            // 
+            playerCard7.Location = new Point(567, 184);
+            playerCard7.Name = "playerCard7";
+            playerCard7.Size = new Size(88, 129);
+            playerCard7.TabIndex = 14;
+            playerCard7.TabStop = false;
+            // 
+            // playerCard9
+            // 
+            playerCard9.Location = new Point(834, 184);
+            playerCard9.Name = "playerCard9";
+            playerCard9.Size = new Size(88, 129);
+            playerCard9.TabIndex = 16;
+            playerCard9.TabStop = false;
+            // 
+            // btnNewGame
+            // 
+            btnNewGame.Location = new Point(36, 33);
+            btnNewGame.Name = "btnNewGame";
+            btnNewGame.Size = new Size(96, 45);
+            btnNewGame.TabIndex = 17;
+            btnNewGame.Text = "Surrender";
+            btnNewGame.UseVisualStyleBackColor = true;
+            btnNewGame.Click += btnNewGame_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1067, 504);
-            Controls.Add(splitContainer1);
+            Controls.Add(btnNewGame);
+            Controls.Add(playerCard9);
+            Controls.Add(playerCard8);
+            Controls.Add(playerCard7);
+            Controls.Add(playerCard6);
+            Controls.Add(playerCard5);
+            Controls.Add(playerCard4);
+            Controls.Add(playerCard3);
+            Controls.Add(playerCard2);
+            Controls.Add(playerCard1);
+            Controls.Add(dealerCard2);
+            Controls.Add(dealerCard1);
             Controls.Add(btnDeal);
             Controls.Add(btnStay);
             Controls.Add(btnHit);
-            Controls.Add(lblscoreComputer);
+            Controls.Add(lblDealer);
             Controls.Add(lblscorePlayer);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "Form1";
             Text = "Form1";
-            Paint += Form1_Paint;
-            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
-            splitContainer1.ResumeLayout(false);
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dealerCard1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dealerCard2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerCard9).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
         private Label lblscorePlayer;
-        private Label lblscoreComputer;
+        private Label lblDealer;
         private Button btnHit;
         private Button btnStay;
         private Button btnDeal;
-        private SplitContainer splitContainer1;
+        private PictureBox dealerCard1;
+        private PictureBox dealerCard2;
+        private PictureBox playerCard2;
+        private PictureBox playerCard1;
+        private PictureBox playerCard4;
+        private PictureBox playerCard3;
+        private PictureBox playerCard6;
+        private PictureBox playerCard5;
+        private PictureBox playerCard8;
+        private PictureBox playerCard7;
+        private PictureBox playerCard9;
+        private Button btnNewGame;
     }
 }
--- BlackJack/Form1.cs
+++ BlackJack/Form1.cs
@@ -1,55 +1,299 @@
+using System.Diagnostics;
+
 namespace BlackJack
 {
     public partial class Form1 : Form
     {
-        private readonly int Starting_hand = 2;
-        private readonly int Max_cards = 11;
-        PictureBox p;
-        PictureBox q;
-        Deck deck;
-        Hand player;
-        Hand dealer;
-        int playerScore;
-        int dealerScore;
+
+        public static string DealerSecondCard, PlayerFirstCard, PlayerSecondCard;
+        public static int pScore1, pScore2;
+        public static bool isGameOver;
+        private int playerScore, dealerScore, rnCard, rnSuit, playerCardsCount, cardValue;
+        private int dScore1, dScore2;
+        private string suit, card, dCard;
+
+
         public Form1()
         {
             InitializeComponent();
         }
-        public void NewGame()
+        private void Form1_Load(object sender, EventArgs e)
         {
-            ClearTable();
-            playerScore = 0;
-            dealerScore = 0;
-            Deck deck = new Deck();
-            Hand player = new Hand(2, deck);
-            Hand dealer = new Hand(2, deck);
-            lblscoreComputer.Text = "0";
-            lblscorePlayer.Text = "0";
-
+            StartNewGame();
         }
-
-        private void ClearTable()
+        private void btnNewGame_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            StartNewGame();
         }
 
         private void btnHit_Click(object sender, EventArgs e)
         {
-            Card drawn = deck.DrawCard(player);
-            player.Cards.Add(drawn);
-            player.AddValue(drawn, ref playerScore);
-
+            DealPlayerCard();
         }
 
         private void btnStay_Click(object sender, EventArgs e)
         {
-
+            Stay();
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void StartNewGame()
         {
-            lblscoreComputer.Text = dealerScore.ToString();
-            lblscorePlayer.Text = playerScore.ToString();
+            try
+            {
+                btnHit.Enabled = true;
+                btnStay.Enabled = true;
+
+                isGameOver = false;
+                btnNewGame.Text = "Quit";
+
+                playerCard3.Visible = false;
+                playerCard4.Visible = false;
+                playerCard5.Visible = false;
+                playerCard6.Visible = false;
+                playerCard7.Visible = false;
+                playerCard8.Visible = false;
+                playerCard9.Visible = false;
+
+                lblDealer.Visible = true;
+
+                rnCard = 0;
+                rnSuit = 0;
+                playerScore = 0;
+                dealerScore = 0;
+                playerCardsCount = 0;
+
+                dScore1 = 0;
+                dScore2 = 0;
+                pScore1 = 0;
+                pScore2 = 0;
+
+                suit = null;
+                card = null;
+                dCard = null;
+
+                Random random = new Random();
+
+                dScore1 = random.Next(1, 13);
+                ConvertFaceCardToNumberValue(dScore1);
+                dScore1 = cardValue;
+                dScore2 = random.Next(1, 13);
+                ConvertFaceCardToNumberValue(dScore2);
+                dScore2 = cardValue;
+                ConvertFaceCardToFileName(dScore2);
+
+                dealerScore = dScore1 + dScore2;
+
+                dealerCard1.Image = Image.FromFile("cards/back-red-75-2.png");
+                SelectSuit();
+                dealerCard2.Image = Image.FromFile("cards/" + suit + "-" + card + "-75.png");
+                DealerSecondCard = suit + "-" + card + "-75.png";
+
+                lblDealer.Visible = true;
+                lblDealer.Text = "Dealer score: " + dealerScore;
+
+                if (dealerScore < 21)
+                {
+                    DealCard();
+                    pScore1 = rnCard;
+
+                    ConvertFaceCardToNumberValue(pScore1);
+                    pScore1 = cardValue;
+                    playerCard1.Image = Image.FromFile("cards/" + dCard);
+                    PlayerFirstCard = dCard;
+
+                    DealCard();
+                    pScore2 = rnCard;
+
+                    ConvertFaceCardToNumberValue(pScore2);
+                    pScore2 = cardValue;
+                    playerCard2.Image = Image.FromFile("cards/" + dCard);
+                    PlayerSecondCard = dCard;
+
+                    playerScore = pScore1 + pScore2;
+                    playerCardsCount = 2;
+                }
+                else
+                    GameOver("dealer21");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+        private void DealCard()
+        {
+            Random random = new Random();
+            rnCard = random.Next(1, 13);
+            ConvertFaceCardToFileName(rnCard);
+            ConvertFaceCardToNumberValue(rnCard);
+            SelectSuit();
+            dCard = suit + "-" + card + "-75.png";
+        }
+
+        private void DealPlayerCard()
+        {
+            DealCard();
+            playerCardsCount++;
+
+            if (playerCardsCount == 3)
+            {
+                playerCard3.Image = Image.FromFile("cards/" + dCard);
+                playerCard3.Visible = true;
+            }
+            else if (playerCardsCount == 4)
+            {
+                playerCard4.Image = Image.FromFile("cards/" + dCard);
+                playerCard4.Visible = true;
+            }
+            else if (playerCardsCount == 5)
+            {
+                playerCard5.Image = Image.FromFile("cards/" + dCard);
+                playerCard5.Visible = true;
+            }
+            else if (playerCardsCount == 6)
+            {
+                playerCard6.Image = Image.FromFile("cards/" + dCard);
+                playerCard6.Visible = true;
+            }
+            else if (playerCardsCount == 7)
+            {
+                playerCard7.Image = Image.FromFile("cards/" + dCard);
+                playerCard7.Visible = true;
+            }
+            else if (playerCardsCount == 8)
+            {
+                playerCard8.Image = Image.FromFile("cards/" + dCard);
+                playerCard8.Visible = true;
+            }
+            else if (playerCardsCount == 9)
+            {
+                playerCard9.Image = Image.FromFile("cards/" + dCard);
+                playerCard9.Visible = true;
+            }
+            else
+            {
+                GameOver("tooManyCards");
+            }
+
+            playerScore += cardValue;
+            lblscorePlayer.Text = "Total: " + playerScore;
+
+            if (playerScore > 21)
+                GameOver("playerBust");
+
+        }
+        private void Stay()
+        {
+            btnStay.Enabled = false;
+            btnHit.Enabled = false;
+            lblDealer.Visible = true;
+            lblDealer.Text = "Dealer score: " + dealerScore;
+
+            ConvertFaceCardToNumberValue(dScore1);
+            dScore1 = cardValue;
+            ConvertFaceCardToFileName(dScore1);
+            SelectSuit();
+
+            dealerCard1.Image = Image.FromFile("cards/" + suit + "-" + card + "-75.png");
+
+            if (dealerScore < 17 && dealerScore <= 21)
+            {
+                DealCard();
+                dealerScore += rnCard;
+                Stay();
+            }
+            if (isGameOver == false)
+            {
+                if (dealerScore > 17 && dealerScore <= 21 && dealerScore > playerScore)
+                    GameOver("dealerWins");
+                else if (dealerScore > 21)
+                    GameOver("dealerBust");
+                else if (playerScore > dealerScore)
+                    GameOver("playerWins");
+                else if (playerScore == dealerScore)
+                    GameOver("draw");
+            }
+        }
+        private void GameOver(string Condition)
+        {
+            bool playerWon = false;
+            bool isDraw = false;
+
+            lblDealer.Visible = true;
+            btnHit.Enabled = false;
+            btnStay.Enabled = false;
+
+            isGameOver = true;
+            btnNewGame.Text = "New Game";
+
+            ConvertFaceCardToFileName(dScore1);
+            SelectSuit();
+            dealerCard1.Image = Image.FromFile("cards/" + suit + "-" + card + "-75.png");
+
+            if (Condition == "playerBust")
+                MessageBox.Show("Loss");
+            else if (Condition == "dealer21")
+                MessageBox.Show("The dealer's hand was 21! Game over!", "Loss");
+            else if (Condition == "dealerWins")
+                MessageBox.Show("Shame, the dealer's hand was higher than yours.", "Loss");
+            else if (Condition == "tooManyCards")
+                MessageBox.Show("You used too many cards. You lost.", "Loss");
+            else if (Condition == "playerWins")
+            {
+                MessageBox.Show("Your hand is higher than the dealer's. You win!", "Win");
+                playerWon = true;
+            }
+            else if (Condition == "dealerBust")
+            {
+                MessageBox.Show("The dealer busted. You win!", "Win");
+                playerWon = true;
+            }
+            else if (Condition == "draw")
+            {
+                MessageBox.Show("You tied with the dealer.", "Draw");
+                isDraw = true;
+            }
+        }
+
+        private int ConvertFaceCardToNumberValue(int FaceCard)
+        {
+            if (FaceCard == 11 || FaceCard == 12 || FaceCard == 13)
+                cardValue = 10;
+            else
+                cardValue = FaceCard;
+
+            return cardValue;
+        }
+
+        private string ConvertFaceCardToFileName(int FaceCard)
+        {
+            if (FaceCard == 1)
+                card = "a";
+            else if (FaceCard == 11)
+                card = "j";
+            else if (FaceCard == 12)
+                card = "q";
+            else if (FaceCard == 13)
+                card = "k";
+            else
+                card = FaceCard.ToString();
+
+            return card;
+        }
+        private string SelectSuit()
+        {
+            Random random = new Random();
+            rnSuit = random.Next(1, 4);
+            if (rnSuit == 1)
+                suit = "clubs";
+            else if (rnSuit == 2)
+                suit = "diamonds";
+            else if (rnSuit == 3)
+                suit = "hearts";
+            else if (rnSuit == 4)
+                suit = "spades";
+
+            return suit;
         }
     }
 }
